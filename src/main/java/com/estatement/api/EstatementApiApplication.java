90acc40f4ffb4d19ffebfd80ea1f6e41f268b433
theprashantbhagat <bhagatprashant1997@gmail.com>
2024-01-10 22:18:37 +0530

    project

---
+++ src/main/java/com/estatement/api/EstatementApiApplication.java
@@ -0,0 +1,13 @@
+package com.estatement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatementApiApplication.class, args);
+	}
+
+}
